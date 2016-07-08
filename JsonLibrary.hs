f4a4c1c0acdb15f46594173eeda48dfdf420643b
Hares Faiez <faiez.hrs@gmail.com>
2016-07-08 19:39:00 +0100

    Add getters for the value constructors of JsonValue

--- JsonLibrary.hs
+++ JsonLibrary.hs
@@ -7,8 +7,32 @@ data JsonValue
     | JsonArray  [JsonValue]
       deriving (Eq, Ord, Show)
 
-stringFrom :: JsonValue -> Maybe String
 stringFrom (JsonString wrapped)
   = Just wrapped
 stringFrom _
   = Nothing
+
+numberFrom (JsonNumber wrapped)
+  = Just wrapped
+numberFrom _
+  = Nothing
+
+boolFrom (JsonBool wrapped)
+  = Just wrapped
+boolFrom _
+  = Nothing
+
+objectFrom (JsonObject wrapped)
+  = Just wrapped
+objectFrom _
+  = Nothing
+
+arrayFrom (JsonArray wrapped)
+  = Just wrapped
+arrayFrom _
+  = Nothing
+
+isNull JsonNull
+  = True
+isNull _
+  = False
