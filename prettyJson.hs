47ac961ebb684969668a0dc73426378f6d1fa5cb
Hares Faiez <faiez.hrs@gmail.com>
2016-07-18 19:28:28 +0100

    Format the code

--- prettyJson.hs
+++ prettyJson.hs
@@ -4,9 +4,58 @@ data Doc
   = ToBeDefined
   deriving (Show)
 
-string :: String -> Doc
-string str
+<> :: Doc -> Doc -> Doc
+<>
   = undefined
+
+asDoc :: Char -> Doc
+asDoc
+  = undefined
+
+concatenateDoc :: [Doc] -> Doc
+concatenateDoc documents
+  = undefined
+
+oneCharacter :: Char -> Doc
+oneCharacter input
+  = case lookup input escapes of
+  Just replacement     -> text      replacement
+  Nothing
+    | mustEscape input -> hexEscape input
+    | otherwise        -> asDoc     input
+    where mustEscape input = input < ' ' || input == '\x7f' || input > '\xff'
+
+escapes :: [(Char, String)]
+escapes
+  = zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
+  where ch a b = (a, ['\\', b])
+
+smallHex :: Int -> Doc
+smallHex value
+  =  text "\\u"
+  <> text (replicate (4 - length hex) '0')
+  <> text hex
+  where hex = showHex value ""
+
+astral :: Int -> Doc
+astral n
+  = smallHex (a + 0xd800) <> smallHex (b + 0xdc00)
+  where a = (n `shiftR` 10) .&. 0x3ff
+        b = n .&. 0x3ff
+
+hexEscape :: Char -> Doc
+hexEscape c
+  | d < 0x10000 = smallHex d
+  | otherwise   = astral (d - 0x10000)
+  where d = ord c
+
+enclose :: Char -> Char -> Doc -> Doc           
+enclose opening closing fit
+  = asDoc opening <> fit <> asDoc closing
+
+string :: String -> Doc
+string
+  = enclose '"' '"' . concatenateDoc . map oneCharacter
 
 text :: String -> Doc
 text str
