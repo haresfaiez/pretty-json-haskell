3660bcb0f7d4d093c9978995bcfcbbe7bf5b3450
Hares Faiez <faiez.hrs@gmail.com>
2018-01-16 10:34:00 +0100

    Add output printing

--- prettyJson.hs
+++ prettyJson.hs
@@ -152,3 +152,26 @@ compact aDoc = transform [aDoc]
                   Line                  -> '\n' : transform tail
                   first `Concat` second -> transform (first : second : tail)
                   _      `Union` value  -> transform (value : tail)
+                
+pretty :: Int -> Doc -> String
+pretty width x = best 0 [x]
+  where best col (d:ds) =
+          case d of
+            Empty        -> best col ds
+            Char c       -> c : best (col + 1) ds
+            Text s       -> s ++ best (col + length s) ds
+            Line         -> '\n' : best 0 ds
+            a `Concat` b -> best col (a:b:ds)
+            a `Union` b  -> nicest col (best col (a:ds))
+                                       (best col (b:ds))
+        best _ _ = ""
+        nicest col a b
+          | (width - least) `fits` a = a
+          | otherwise                = b
+          where least = min width col
+
+fits :: Int -> String -> Bool
+w `fits` _ | w < 0 = False
+w `fits` ""        = True
+w `fits` ('\n':_)  = True
+w `fits` (c:cs)    = (w - 1) `fits` cs
